677632e98526e484e6390f94532c33255c42af59
khanadnan701-debug <khan.adnan701@gmail.com>
2026-09-10 20:39:34 +0530

    Add Cloudinary spice product override

---
+++ spice-products-20260910.js
@@ -0,0 +1,189 @@
+(() => {
+  const spiceProducts20260910 = [
+    {
+      category: 'spices',
+      title: 'Shama Amchur Powder (Mango Powder)',
+      pack: '3377C - 100g x 20',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789051969/aamchur_pwdr.png'
+    },
+    {
+      category: 'spices',
+      title: 'Shama Bay Leaves',
+      pack: '50g x 20\n200g x 10',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789051971/bay_leaves.png'
+    },
+    {
+      category: 'spices',
+      title: 'Shama Black Cardamom',
+      pack: '50g x 20\n200g x 10\n750g x 6',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789051969/black_cardemom.png'
+    },
+    {
+      category: 'spices',
+      title: 'Shama Black Mustard Seed',
+      pack: '100g x 20\n400g x 10\n1kg x 6',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789051970/black_mustard_seed.png'
+    },
+    {
+      category: 'spices',
+      title: 'Shama Black Pepper Powder',
+      pack: '100g x 20\n400g x 10\n1kg x 6',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789051979/black_ppr_pwdr.png'
+    },
+    {
+      category: 'spices',
+      title: 'Shama Black Pepper Whole',
+      pack: '50g x 20\n200g x 10\n750g x 6',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789051972/black_ppr_whole.png'
+    },
+    {
+      category: 'spices',
+      title: 'Shama Chilli Crushed',
+      pack: '100g x 20\n400g x 10\n1kg x 6',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789051974/chilli_crushed.png'
+    },
+    {
+      category: 'spices',
+      title: 'Shama Chilli Powder',
+      pack: '100g x 20\n400g x 10\n1kg x 6',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789051973/chilli_powder.png'
+    },
+    {
+      category: 'spices',
+      title: 'Shama Chilli Powder Extra Hot',
+      pack: '100g x 20\n400g x 10\n1kg x 6',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789051972/chilli_powder_ex_hot.png'
+    },
+    {
+      category: 'spices',
+      title: 'Shama Dalchini Whole (Cinnamon Sticks)',
+      pack: '3360C - 50g x 20\n3582C - 200g x 10\n3766C - 400g x 6\n2486A - 1.5kg x 1',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789051975/cinnemon_sticks.png'
+    },
+    {
+      category: 'spices',
+      title: 'Shama Cloves Whole',
+      pack: '3308A - 50g x 20\n3520C - 200g x 10\n3704C - 750g x 6',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789051979/cloves_whole.png'
+    },
+    {
+      category: 'spices',
+      title: 'Shama Dhania Powder (Coriander Powder)',
+      pack: '100g x 20\n400g x 10\n1kg x 6',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789051977/dhania_powdr.png'
+    },
+    {
+      category: 'spices',
+      title: 'Shama Dhania Whole (Coriander Seeds)',
+      pack: '100g x 20\n400g x 10\n1kg x 6',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789051981/dhania_whole.png'
+    },
+    {
+      category: 'spices',
+      title: 'Shama Fennel Seeds',
+      pack: '100g x 20\n400g x 10\n1kg x 6',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789051977/fennel.png'
+    },
+    {
+      category: 'spices',
+      title: 'Shama Garam Masala',
+      pack: '100g x 20\n400g x 10\n1kg x 6',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789051970/garam_masala.png'
+    },
+    {
+      category: 'spices',
+      title: 'Shama Garam Masala Whole',
+      pack: '100g x 20\n400g x 10\n1kg x 6',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789051970/garam_masala_whole.png'
+    },
+    {
+      category: 'spices',
+      title: 'Shama Garlic Powder',
+      pack: '100g x 20\n400g x 10\n1kg x 6',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789051971/garlc_powde.png'
+    },
+    {
+      category: 'spices',
+      title: 'Shama Ginger Powder',
+      pack: '100g x 20\n400g x 10\n1kg x 6',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789051974/ginger_powder.png'
+    },
+    {
+      category: 'spices',
+      title: 'Shama Green Cardamom',
+      pack: '3346C - 50g x 20\n3568C - 200g x 10\n3742C - 750g x 6',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789051975/green_cardemom.png'
+    },
+    {
+      category: 'spices',
+      title: 'Shama Haldi Powder (Turmeric Powder)',
+      pack: '3230C - 100g x 20\n3452C - 400g x 10\n3643C - 1kg x 6',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789051977/haldi_powder.png'
+    },
+    {
+      category: 'spices',
+      title: 'Shama Jeera Powder (Cumin)',
+      pack: '3184C - 100g x 20\n3384C - 400g x 10\n3599C - 1kg x 6',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789051978/jeera_pwdr.png'
+    },
+    {
+      category: 'spices',
+      title: 'Shama Jeera Whole (Cumin)',
+      pack: '3292C - 100g x 20\n3513C - 400g x 10\n3698C - 1kg x 6',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789051980/jeera_whole.png'
+    },
+    {
+      category: 'spices',
+      title: 'Shama Kalonji (Nigella Seeds)',
+      pack: '2950C - 100g x 20\n2967C - 300g x 10',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789051981/kalonji.png'
+    },
+    {
+      category: 'spices',
+      title: 'Shama Ajwain (Lovage Seeds)',
+      pack: '2806C - 100g x 20\n2882A - 300g x 10\n2875A - 1kg x 6',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789051982/lovage_seed.png'
+    },
+    {
+      category: 'spices',
+      title: 'Shama Madras Curry Hot',
+      pack: '100g x 20\n400g x 10\n1kg x 6',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789051984/madras_curry_hot.png'
+    },
+    {
+      category: 'spices',
+      title: 'Shama Madras Curry Mild',
+      pack: '100g x 20\n400g x 10\n1kg x 6',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789051983/madras_curry_mild.png'
+    },
+    {
+      category: 'spices',
+      title: 'Shama Methi Seeds (Fenugreek)',
+      pack: '3285C - 100g x 20\n3506C - 400g x 10\n3681C - 1kg x 6',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789051983/methi_seeds.png'
+    },
+    {
+      category: 'spices',
+      title: 'Shama Saunf (Fennel Seeds)',
+      pack: '3247C - 100g x 20\n3469C - 400g x 10\n3650C - 1kg x 6',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789051985/saunf.png'
+    },
+    {
+      category: 'spices',
+      title: 'Shama Star Anise',
+      pack: '2868C - 50g x 20',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789051986/star_anise.png'
+    },
+    {
+      category: 'spices',
+      title: 'Shama Tandoori Masala',
+      pack: '3223C - 100g x 20\n3445C - 400g x 10\n3636C - 1kg x 6',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789051968/tandoori_masala.png'
+    }
+  ];
+
+  if (typeof productData !== 'undefined' && Array.isArray(productData)) {
+    const nonSpiceProducts = productData.filter((item) => item.category !== 'spices');
+    productData.splice(0, productData.length, ...nonSpiceProducts, ...spiceProducts20260910);
+  }
+})();
