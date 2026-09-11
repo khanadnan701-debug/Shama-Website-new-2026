da9799c241ad91d54aaf09f3ee43a31457c95707
khanadnan701-debug <khan.adnan701@gmail.com>
2026-09-12 01:12:47 +0530

    Update lentils catalogue with Flour-Lentiles Cloudinary assets

--- lentil-products-20260911.js
+++ lentil-products-20260911.js
@@ -3,26 +3,38 @@
   if (document.body.dataset.category !== 'flour') return;
   if (typeof productData === 'undefined' || !Array.isArray(productData)) return;
 
+  const base = 'https://res.cloudinary.com/wy4nkkqq/image/upload/';
   const lentilProducts = [
-    { category: 'flour', title: 'Shama Desi Black Eye Beans', pack: '1kg', image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789131919/Shama_desi_black_eye_beans_1kg.png' },
-    { category: 'flour', title: 'Shama Desi Masoor Dal Whole', pack: '1kg', image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789131920/Shama_desi_masoor_dal_whole_1kg.png' },
-    { category: 'flour', title: 'Shama Desi Moong Dal', pack: '1kg', image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789131920/Shama_desi_moong_dal_1kg.png' },
-    { category: 'flour', title: 'Shama Desi Moong Whole', pack: '1kg', image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789131920/Shama_desi_moong_whole_1kg.png' },
-    { category: 'flour', title: 'Shama Desi Masoor Dal', pack: '1kg', image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789131921/Shama_desi_masoor_dal_1kg.png' },
-    { category: 'flour', title: 'Shama Desi Dal Whole', pack: '1kg', image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789131921/Shama_desi_dal_whole_1kg.png' },
-    { category: 'flour', title: 'Shama Desi Chana Dal', pack: '1kg', image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789131921/Shama_desi_chana_dal_1kg.png' },
-    { category: 'flour', title: 'Shama Desi Kidney Beans', pack: '1kg', image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789131922/Shama_desi_kidney_beans_1kg.png' },
-    { category: 'flour', title: 'Shama Desi Rajma', pack: '1kg', image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789131922/Shama_desi_rajma_1kg.png' },
-    { category: 'flour', title: 'Shama Desi Urad Dal', pack: '1kg', image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789131923/Shama_desi_urad_dal_1kg.png' },
-    { category: 'flour', title: 'Shama Desi Toor Dal', pack: '1kg', image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789131923/Shama_desi_toor_dal_1kg.png' },
-    { category: 'flour', title: 'Shama Desi White Chick Peas', pack: '1kg', image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789131924/Shama_desi_white_chick_peas_1kg.png' },
-    { category: 'flour', title: 'Shama Desi Kala Chana', pack: '1kg', image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789131925/Shama_desi_kala_chana_1kg.png' },
-    { category: 'flour', title: 'Shama Desi Urad Whole', pack: '1kg', image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789131926/Shama_desi_urad_whole_1kg.png' },
-    { category: 'flour', title: 'Shama Desi Urad Dal Chilka', pack: '1kg', image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789131926/Shama_desi_urad_dal_chilka_1kg.png' },
-    { category: 'flour', title: 'Shama Desi Urad Gota', pack: '1kg', image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789131927/Shama_desi_urad_gota1kg.png' },
-    { category: 'flour', title: 'Shama Desi Yellow Split Peas', pack: '1kg', image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789131927/Shama_desi_yellow_split_peas_1kg.png' },
-    { category: 'flour', title: 'Shama Masoor Dal', pack: '5kg', image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789131928/Shama_masoor_dal_5kg.png' }
-  ];
+    ['Shama Desi Black Eye Beans','Shama_desi_black_eye_beans_1kg'],
+    ['Shama Desi Urad Dal','Shama_desi_urad_dal_1kg_2'],
+    ['Shama Desi Chana Dal','Shama_desi_chana_dal_1kg'],
+    ['Shama Desi Toor Dal','Shama_desi_toor_dal_1kg'],
+    ['Shama Desi Kidney Beans','Shama_desi_kidney_beans_1kg'],
+    ['Shama Desi Masoor Dal','Shama_desi_masoor_dal_1kg'],
+    ['Shama Desi Dal Whole','Shama_desi_dal_whole_1kg'],
+    ['Shama Desi Urad Dal','Shama_desi_urad_dal_1kg'],
+    ['Shama Desi Urad Whole','Shama_desi_urad_1kg'],
+    ['Shama Desi Kala Chana','Shama_desi_kala_chana_1kg'],
+    ['Shama Desi Moong Whole','Shama_desi_moong_whole_1kg'],
+    ['Shama Desi Moong Dal','Shama_desi_moong_dal_1kg'],
+    ['Shama Desi Masoor Dal Whole','Shama_desi_masoor_dal_whole_1kg'],
+    ['Shama Desi Rajma','Shama_desi_rajma_1kg'],
+    ['Shama Desi White Chick Peas','Shama_desi_white_chick_peas_1kg'],
+    ['Shama Desi Yellow Split Peas','Shama_desi_yellow_split_peas_1kg'],
+    ['Shama Desi Masoor Dal','Shama_desi_masoor_dal_1kg_2'],
+    ['Shama Desi Moong Dal','Shama_desi_moong_dal_1kg_2'],
+    ['Shama Desi Chana Dal','Shama_desi_chana_dal_1kg_2'],
+    ['Shama Desi Urad Dal Chilka','Shama_desi_urad_dal_chilka_1kg'],
+    ['Shama Masoor Dal','Shama_masoor_dal_5kg'],
+    ['Shama Desi Urad Gota','Shama_desi_urad_gota1kg'],
+    ['Shama Desi Urad Whole','Shama_desi_urad_whole_1kg'],
+    ['Shama Desi Toor Dal','Shama_desi_toor_dal_1kg_2']
+  ].map(([title, id]) => ({
+    category:'flour',
+    title,
+    pack:'1kg',
+    image:`${base}v1789155082/${id}.png`
+  }));
 
   const nonFlourProducts = productData.filter(item => item.category !== 'flour');
   productData.splice(0, productData.length, ...nonFlourProducts, ...lentilProducts);
@@ -41,10 +53,6 @@
     }
   };
 
-  if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', rerender, { once: true });
-  } else {
-    setTimeout(rerender, 0);
-  }
-  window.addEventListener('load', rerender, { once: true });
+  rerender();
+  window.addEventListener('load', rerender, { once:true });
 })();
