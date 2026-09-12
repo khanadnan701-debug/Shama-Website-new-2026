f0ebc39ab15717db2e96e0eaaa5386eaf3fbf44c
khanadnan701-debug <khan.adnan701@gmail.com>
2026-09-12 22:38:51 +0530

    Show all Cloudinary dry fruit assets

--- dry-fruit-images-20260912.js
+++ dry-fruit-images-20260912.js
@@ -1,27 +1,101 @@
 (() => {
   'use strict';
 
-  if (typeof productData === 'undefined') return;
+  if (typeof productData === 'undefined' || !Array.isArray(productData)) return;
 
-  const dryFruitImages = {
-    'Shama Raw Almond': 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789232085/Shama_Raw_almonds_100gm.png',
-    'Shama Raisin Golden Iran': 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789232080/Shama_golden_raisin_100g.png',
-    'Shama Coconut Powder': 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789232079/Shama_coconut_powder_400gm.png',
-    'Shama Pistachio Husked': 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789232082/Shama_pistatio_husked_100g.png',
-    'Shama Broken Cashew': 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789232079/Shama_broken_cajou_800gm.png',
-    'Shama Whole Cashews': 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789232083/Shama_Raw_cashews_100gm.png'
-  };
-
-  for (let index = productData.length - 1; index >= 0; index -= 1) {
-    const item = productData[index];
-    if (item.category === 'dry-fruits' && !dryFruitImages[item.title]) {
-      productData.splice(index, 1);
+  const dryFruitProducts = [
+    {
+      category: 'dry-fruits',
+      title: 'Shama Coconut Powder',
+      pack: '400g',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789232079/Shama_coconut_powder_400gm.png'
+    },
+    {
+      category: 'dry-fruits',
+      title: 'Shama Roasted Gram & Makhana',
+      pack: '200g Jar',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789232079/Shama_roasted_gram_and_makhana_200g_jar.png'
+    },
+    {
+      category: 'dry-fruits',
+      title: 'Shama Broken Cashew',
+      pack: '800g',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789232079/Shama_broken_cajou_800gm.png'
+    },
+    {
+      category: 'dry-fruits',
+      title: 'Shama Golden Raisin',
+      pack: '100g',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789232080/Shama_golden_raisin_100g.png'
+    },
+    {
+      category: 'dry-fruits',
+      title: 'Shama Golden Raisin',
+      pack: '200g',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789232081/Shama_golden_raisin_200g.png'
+    },
+    {
+      category: 'dry-fruits',
+      title: 'Shama Raw Almonds',
+      pack: '800g',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789232081/Shama_Raw_almonds_800gm.png'
+    },
+    {
+      category: 'dry-fruits',
+      title: 'Shama Golden Raisin',
+      pack: '500g',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789232081/Shama_golden_raisin_500g.png'
+    },
+    {
+      category: 'dry-fruits',
+      title: 'Shama Roasted Chana',
+      pack: '600g',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789232081/Shama_roasted_chana_600gm.png'
+    },
+    {
+      category: 'dry-fruits',
+      title: 'Shama Pistachio Husked',
+      pack: '100g',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789232082/Shama_pistatio_husked_100g.png'
+    },
+    {
+      category: 'dry-fruits',
+      title: 'Shama Raw Cashews',
+      pack: '100g',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789232083/Shama_Raw_cashews_100gm.png'
+    },
+    {
+      category: 'dry-fruits',
+      title: 'Shama Pistachio Roasted & Salted',
+      pack: '100g',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789232083/Shama_pistatio_roasted_salted_100g.png'
+    },
+    {
+      category: 'dry-fruits',
+      title: 'Shama Raisin Munakka',
+      pack: '100g',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789232083/Shama_raisin_munakka_100g.png'
+    },
+    {
+      category: 'dry-fruits',
+      title: 'Shama Desiccated Coconut',
+      pack: '1kg',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789232085/Shama_desicated_coconut_1kg.png'
+    },
+    {
+      category: 'dry-fruits',
+      title: 'Shama Raw Almonds',
+      pack: '100g',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789232085/Shama_Raw_almonds_100gm.png'
+    },
+    {
+      category: 'dry-fruits',
+      title: 'Shama Green Raisins',
+      pack: '100g',
+      image: 'https://res.cloudinary.com/wy4nkkqq/image/upload/v1789232085/Shama_Raisin_green_100gm.png'
     }
-  }
+  ];
 
-  productData.forEach(item => {
-    if (item.category !== 'dry-fruits') return;
-    const image = dryFruitImages[item.title];
-    if (image) item.image = image;
-  });
+  const otherProducts = productData.filter(item => item.category !== 'dry-fruits');
+  productData.splice(0, productData.length, ...otherProducts, ...dryFruitProducts);
 })();
